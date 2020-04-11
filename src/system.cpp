074ba51a4ae090faeb8d1509c4c696d10a942ecf
flsmr <letmepass@gmx.com>
2020-04-11 12:41:25 +0200

    added system constructor

--- src/system.cpp
+++ src/system.cpp
@@ -7,12 +7,18 @@
 #include "process.h"
 #include "processor.h"
 #include "system.h"
+#include "linux_parser.h"
 
 using std::set;
 using std::size_t;
 using std::string;
 using std::vector;
 
+// System constructor
+System::System() {
+    operatingSystem_ = LinuxParser::OperatingSystem();
+    kernel_ = LinuxParser::Kernel();
+}
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
@@ -20,13 +26,13 @@ Processor& System::Cpu() { return cpu_; }
 vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return kernel_; }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return operatingSystem_; }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
